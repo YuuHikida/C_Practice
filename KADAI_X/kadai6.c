e51e2c3224722cbb9e954905a4716c0ef4676496
Yuu Hikida <ponponda0103@gmail.com>
2024-06-28 11:19:57 +0900

    kadais

--- KADAI_X/kadai6.c
+++ KADAI_X/kadai6.c
@@ -12,14 +12,18 @@ main( void )
 
     float intPart_float = floor( i );   //3
     double intPart_double = floor( j ); //3
+    //円周率
+    int int_x = 3.123456789;
 
-    int int_x = 3.123456789; 
+    //もしくは以下
+
     printf( "intPart_float:(%f)\n", intPart_float );
     printf( "intPart_double:(%g)\n", intPart_double );//3
 
     float  fracPart_float  = i - intPart_float;
     double fracPart_double = j - intPart_double;
-
+    float x = fracPart_double - int_x ;
+    
     printf( "floatの小数点表示:(%f)\n", fracPart_float ); //0.123456717
     printf( "doubleの小数点表示:(%g)\n", fracPart_float ); //0.12345678900000001
     
