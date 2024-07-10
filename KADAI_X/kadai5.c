55e9df75bb92cfb4a135c37e5f4408b06d4e5b69
Yuu Hikida <ponponda0103@gmail.com>
2024-07-10 20:56:15 +0900

    practice

--- KADAI_X/kadai5.c
+++ KADAI_X/kadai5.c
@@ -1,10 +1,22 @@
+#include <string.h> 
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int
 main ( void )
 {
-    char *prt = ( char* )0x100;
-    char *prt2 = prt --;
-    printf("%c",*prt2);
+    // char *prt = ( char* )0x100;
+    // char *prt2 = prt --;
+    // printf("%c",*prt2);
+
+    int array[10];
+    size_t xx_size = sizeof(array)/sizeof(array[0]);
+    
+    // char *x = (char *)malloc(xx_size);
+    char *x;
+    memset(x,0x00,xx_size);
+    printf("thisis=%p",x);
+    return 0;
 }
 
