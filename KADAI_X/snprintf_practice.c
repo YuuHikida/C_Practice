c949ce1b9230ec96a9cdd3a25b85034a7a839085
Yuu Hikida <ponponda0103@gmail.com>
2024-08-01 21:14:50 +0900

    snprintf動作確認-ちょいつけたし

--- KADAI_X/snprintf_practice.c
+++ KADAI_X/snprintf_practice.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 
-int main()
+void test_A()
+{
+    char x[100];
+    char test[50];
+    char y[] = "こんにちは、私の名前はジョンスミスです。１０サイズ以上の文字列を作成します";
+    char o[10] = "xyz";
+    char element[] = "abc";
+    (void)snprintf(x, sizeof(x), "%.*s/%.*s",10000, o, 50 , element);
+    
+    printf("ans=%s\n",x);
+}
+void test_B()
 {
     char x[10];
     char test[50];
     char y[] = "こんにちは、私の名前はジョンスミスです。１０サイズ以上の文字列を作成します";
     char o[10] = "xyz";
-    char element[] = "abcdefghijkmln";
-    (void)snprintf(test, sizeof(x), "%.*s/%.*s",5 , o, 50 , element);
+    char element[] = "abcefghijkmln";
+    (void)snprintf(x, sizeof(x), "%.*s/%.*s",1000, o, 50 , element);
     
+    printf("ans=%s\n",x);
+}
+int main()
+{
+    // test_A();
+    test_B();
+
     // // (void)snprintf(x, sizeof(x), "%.*s", (int)sizeof(x), y);
     // (void)snprintf(x, sizeof(x), "%.*s", (int)sizeof(x),
     //      "こんにちは、私の名前はジョンスミスです。１０サイズ以上の文字列を作成します");
     
-    printf("ans=%s",test);
+    
     /*
     //出力結果:Korega=こんに%
     (void)snprintf(x,sizeof(x),y);
